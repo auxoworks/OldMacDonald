c753dbc8c9b9b51aaa3bc636b712892ccbaa1ffc
auxoworks <tjlee815@gmail.com>
2015-10-29 09:52:48 -0700

    fixed duplicate

--- NamedCow.java
+++ NamedCow.java
@@ -1,7 +1,5 @@
 class NamedCow extends Cow
 {
-	private String myType;
-	private String mySound;
 	private String myName;
 public NamedCow(String type,String sound,String name){
 		myType=type;
@@ -14,7 +12,5 @@ myType="cow";
 mySound="moo";
 myName="John Cena";
 }
-     public String getSound(){return mySound;}     
-     public String getType(){return myType;} 
      public String getName(){return myName;} 
 }
